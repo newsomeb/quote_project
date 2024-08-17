2b6b5f71f98f4cedb9d9a06eee29e5712fe82aaf
newsomeb <122328278+newsomeb@users.noreply.github.com>
2024-08-17 09:28:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1315,5 +1315,5 @@ def match_quote_in_context(quote, context):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
 
